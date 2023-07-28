e7d6914aa08dce70a48ddc242a22ed1acca8ba01
bonzo4 <alonzo.sabino814@gmail.com>
2023-07-28 15:10:05 -0500

    optimize fix

--- utils/news.ts
+++ utils/news.ts
@@ -6,9 +6,7 @@ export async function getNews(): Promise<News[]> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/news`)
 
   if (res.status !== 200) {
-    const data = await res.json()
-    console.error(res)
-    throw new Error(`Failed to load news ${data.error}`)
+    throw new Error(`Failed to load news`)
   }
 
   return res.json()
