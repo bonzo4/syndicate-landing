5601390662b15d34d1b56141f523f9bd75950790
bonzo4 <alonzo.sabino814@gmail.com>
2023-07-28 15:03:10 -0500

    optimize fix

--- utils/news.ts
+++ utils/news.ts
@@ -5,8 +5,9 @@ type News = Database["public"]["Tables"]["news_previews"]["Row"]
 export async function getNews(): Promise<News[]> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/news`)
 
-  if (!res.ok) {
-    throw new Error(`Failed to load news ${(await res.json()).error}`)
+  if (res.status !== 200) {
+    const error = await res.json()
+    throw new Error(`Failed to load news ${error.error}`)
   }
 
   return res.json()
