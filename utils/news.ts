c5d7cbb52d1062f911d1bd70126436dd69fc1b46
hvKalista <alonzo.sabino814@gmail.com>
2023-09-24 19:33:10 -0500

    fix

--- utils/news.ts
+++ utils/news.ts
@@ -3,7 +3,7 @@ import { Database } from '@/types/supabase';
 type News = Database['public']['Tables']['news_previews']['Row'];
 
 export async function getNews(): Promise<News[]> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/news`);
+  const res = await fetch(`/api/news`);
 
   if (res.status !== 200) {
     throw new Error(`Failed to load news`);
