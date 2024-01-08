43a3d76b01b1d476bfa7d2d66ee4c52a8a0e67ed
bonzo4 <alonzo.sabino814@gmail.com>
2024-01-07 20:52:11 -0600

    fixed nft claim

--- utils/subscriber.ts
+++ utils/subscriber.ts
@@ -5,7 +5,7 @@ type Subscriber = {
 export async function getSubscriber(
   subscriberId: string
 ): Promise<Subscriber | null> {
-  const url = `https://api.beehiiv.com/v2/publications/${process.env.BEE_HIIV_ID}/subscriptions/by_subscriber_id/sub_${subscriberId}`;
+  const url = `https://api.beehiiv.com/v2/publications/${process.env.BEE_HIIV_ID}/subscriptions/by_subscriber_id/${subscriberId}`;
   const options = {
     method: 'GET',
     headers: {
