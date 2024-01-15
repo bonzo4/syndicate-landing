6dd5ae7b0ba63838343c9e28d4d017b9181befb3
bonzo4 <alonzo.sabino814@gmail.com>
2024-01-15 13:29:22 -0600

    claim fix

--- app/api/newsletter/claim/route.ts
+++ app/api/newsletter/claim/route.ts
@@ -67,10 +67,10 @@ export async function GET(request: NextRequest) {
       'https://passport.underdogprotocol.com/syndicate'
     );
   } catch (error: any) {
-    // return NextResponse.json({ error: error.message }, { status: 400 });
+    return NextResponse.json({ error: error.message }, { status: 400 });
     console.error(error.message);
-    return NextResponse.redirect(
-      'https://passport.underdogprotocol.com/syndicate'
-    );
+    // return NextResponse.redirect(
+    //   'https://passport.underdogprotocol.com/syndicate'
+    // );
   }
 }
