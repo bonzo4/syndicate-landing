8414ceb2123b955ed51664ef39ae7d4bdf95ca29
hvKalista <alonzo.sabino814@gmail.com>
2024-02-19 15:32:32 -0600

    news feed fix

--- utils/news.ts
+++ utils/news.ts
@@ -3,12 +3,12 @@ import { Database } from '@/types';
 type News = Database['public']['Tables']['news_previews']['Row'];
 
 export async function getNews(): Promise<News[]> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/news`);
+  //   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/news`);
 
-  const data = await res.json();
-  if (res.status !== 200) {
-    return [];
-  }
+  //   const data = await res.json();
+  //   if (res.status !== 200) {
+  //     return [];
+  //   }
 
-  return data;
+  return [];
 }
