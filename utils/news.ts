8a513a7649de23b0d0937ec95b0c2cd67cfc6d1e
hvKalista <alonzo.sabino814@gmail.com>
2024-02-19 15:30:39 -0600

    news feed fix

--- utils/news.ts
+++ utils/news.ts
@@ -6,9 +6,9 @@ export async function getNews(): Promise<News[]> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/news`);
 
   const data = await res.json();
-  //   if (res.status !== 200) {
-  //     throw new Error(`Failed to load news`);
-  //   }
+  if (res.status !== 200) {
+    return [];
+  }
 
   return data;
 }
